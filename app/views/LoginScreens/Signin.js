42ea0aa06e7ff46a2a9f980c6070849daafa61c6
Bibish <amb.deyna@gmail.com>
2018-12-16 15:43:09 +0100

    gnak

--- app/views/LoginScreens/Signin.js
+++ app/views/LoginScreens/Signin.js
@@ -1,35 +1,32 @@
 import React from "react";
-import { Text, AsyncStorage } from "react-native";
-import { Container, Content, Form, Item, Input, Button } from "native-base";
-
+import {Text, AsyncStorage} from "react-native";
+import {Col, Row, Grid} from "react-native-easy-grid";
+import {Input, Button} from 'react-native-elements';
 
 export default class SignInScreen extends React.Component {
-  signInAsync = async () => {
-    await AsyncStorage.setItem("userToken", "abc");
-    const { navigation } = this.props;
-    navigation.navigate("App");
-  };
+    //onPress={this.signInAsync}
+    signInAsync = async() => {
+        await AsyncStorage.setItem("userToken", "abc");
+        const {navigation} = this.props;
+        navigation.navigate("App");
+    };
 
-  render() {
-    return (
-      <Container>
-        <Content>
-          <Form>
-            <Item>
-              <Input placeholder="Username" />
-            </Item>
-            <Item last>
-              <Input placeholder="Password" />
-            </Item>
-          </Form>
-          <Button rounded success onPress={this.signInAsync}>
-            <Text>Log in</Text>
-          </Button>
-          <Button rounded info>
-            <Text>SIgn in</Text>
-          </Button>
-        </Content>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Grid>
+                <Row size={10}/>
+                <Row size={30}>
+                    <Row size={10} >
+                        <Input placeholder='user'/>
+                    </Row>
+                    <Row size={10}>
+                        <Input placeholder='password'/>
+                    </Row>
+                    
+                </Row>
+                <Button title='connect' onPress={this.signInAsync}/>
+                <Row/>
+            </Grid>
+        );
+    }
 }
