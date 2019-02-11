95ec78e941a34a3c16081f13cd8336bc0a5a41fa
bibish <amb.deyna@gmail.com>
2019-02-11 22:09:04 +0100

    add trucs

--- app/views/LoginScreens/Signin.js
+++ app/views/LoginScreens/Signin.js
@@ -1,32 +1,58 @@
 import React from "react";
-import {Text, AsyncStorage} from "react-native";
-import {Col, Row, Grid} from "react-native-easy-grid";
-import {Input, Button} from 'react-native-elements';
+import { Text, AsyncStorage, StyleSheet, View } from "react-native";
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { Input } from "react-native-elements";
+
 
 export default class SignInScreen extends React.Component {
-    //onPress={this.signInAsync}
-    signInAsync = async() => {
-        await AsyncStorage.setItem("userToken", "abc");
-        const {navigation} = this.props;
-        navigation.navigate("App");
-    };
+  // onPress={this.signInAsync}
 
-    render() {
-        return (
-            <Grid>
-                <Row size={10}/>
-                <Row size={30}>
-                    <Row size={10} >
-                        <Input placeholder='user'/>
-                    </Row>
-                    <Row size={10}>
-                        <Input placeholder='password'/>
-                    </Row>
-                    
-                </Row>
-                <Button title='connect' onPress={this.signInAsync}/>
-                <Row/>
-            </Grid>
-        );
-    }
+  signInAsync = async () => {
+    await AsyncStorage.setItem("userToken", "abc");
+    const { navigation } = this.props;
+    navigation.navigate("App");
+  };
+
+  render() {
+    return (
+      <Grid>
+        <Row size={35} style={loginStyle.blackBG}>
+          <Col size={1}>
+            <View style={{ alignItems: "center" }}>
+              <Text style={loginStyle.text}>hello there</Text>
+            </View>
+          </Col>
+        </Row>
+        <Row size={65} style={loginStyle.redBG}>
+          <Col size={50}>
+            <Input
+              placeholder="lol"
+            />
+          </Col>
+          <Col size={50}>
+            <Input
+              placeholder="password"
+            />
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
 }
+
+const loginStyle = StyleSheet.create({
+  blackBG: {
+    backgroundColor: "#999"
+  },
+  redBG: {
+    backgroundColor: "red"
+  },
+  text: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 30,
+    paddingTop: 50
+  }
+});
